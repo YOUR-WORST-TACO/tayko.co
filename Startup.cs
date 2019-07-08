ac586915baf3c77a6a5c957c13b037cc009b5768
YOUR-WORST-TACO <stephen@tafoya.dev>
2019-07-07 21:57:15 -0600

    Testing blog structure

--- Startup.cs
+++ Startup.cs
@@ -38,7 +38,7 @@ namespace Tayko.co
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -46,7 +46,8 @@ namespace Tayko.co
             {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
-            }
+            }*/
+            app.UseDeveloperExceptionPage();
             //app.UseExceptionHandler("/Error");
             //app.UseHsts();
             //app.UseDeveloperExceptionPage();
