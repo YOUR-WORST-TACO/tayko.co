a13f3534b37ca63c4408478a61b9d26bf77e411b
YOUR-WORST-TACO <stephen@tafoya.dev>
2019-07-07 22:31:34 -0600

    I screwed it up, but whatever

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -8,11 +8,32 @@ namespace Tayko.co.Controllers
         // GET
         public IActionResult Article(string article)
         {
+            
             if (article != null)
             {
-                
                 return View($"Articles/{article}");
             }
+
+            string[,] articles =
+            {
+                {
+                    "articles-how-to",
+                    "Articles How To",
+                    "How to make articles, an insightful look at article creation"
+                },
+                {
+                    "how-to-article",
+                    "How to Article",
+                    "Another take on making articles, not as insightful, but could incite rage and anger"
+                },
+                {
+                    "my-test-article",
+                    "My Test Article",
+                    "A horrid example of an article, do not look at this"
+                }
+            };
+
+            ViewBag.ArticleBag = articles;
             
             return View("Articles");
         }
