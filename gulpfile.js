9732c92986e603f5792798177edbbbfbfd03807d
your-worst-taco <stephentafoya@outlook.com>
2019-08-07 09:35:03 -0600

    Implemented gulp rename to create main.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
+const rename = require('gulp-rename');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', () => {
+    gulp.src('Styles/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('wwwroot/css/'));
     return gulp.src('Styles/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
         .pipe(gulp.dest('wwwroot/css/'));
 });
 
